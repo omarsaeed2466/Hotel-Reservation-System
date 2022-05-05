51f16b8c15c4759698d3fb8b70e8b5dbb0567638
omar saeed <os83575@gmail.com>
2022-05-05 19:08:46 +0200

    create project

---
+++ Back-end/src/main/java/com/spring/raustrant/raustrant/BackEndApplication.java
@@ -0,0 +1,13 @@
+package com.spring.raustrant.raustrant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackEndApplication.class, args);
+    }
+
+}
---
+++ Back-end/src/test/java/com/spring/raustrant/raustrant/BackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.raustrant.raustrant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
